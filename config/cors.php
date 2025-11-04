49681e6e890151819a2af3594079dcc72b8a6152
Kirills Ustalovs <kirils.u@rdvit.com>
2025-11-05 00:12:09 +0200

    Removed unused files

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => array_map(
+        fn($domain) => str_starts_with($domain, 'http') ? $domain : "http://{$domain}",
+        explode(',', env('ALLOWED_DOMAINS', 'localhost:3000,localhost:5173,127.0.0.1:3000,127.0.0.1:5173'))
+    ),
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +32,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
--- config/sanctum.php
+++ config/sanctum.php
@@ -15,9 +15,9 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+    'stateful' => explode(',', env('ALLOWED_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,localhost:5173,127.0.0.1,127.0.0.1:8000,127.0.0.1:3000,127.0.0.1:5173,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
